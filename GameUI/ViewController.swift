5a5e2c1991b93b93e1092c4d00dd4850d4934333
otto huang <otto@avc.fund>
2018-11-19 22:48:16 +0800

    Initial Commit

---
+++ GameUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameUI
+//
+//  Created by Otto Huang on 2018/11/19.
+//  Copyright © 2018 Otto Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
